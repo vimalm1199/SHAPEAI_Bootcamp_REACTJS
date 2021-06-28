f7253e6823dddcda16bc4635d10e52916a1d0128
vimalm1199 <86589609+vimalm1199@users.noreply.github.com>
2021-06-28 12:03:20 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>CopyRight by ShapeAi @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
